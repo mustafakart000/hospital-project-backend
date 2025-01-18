08bc72799c7f0aa0e9ec1305469aaadc2bef591c
Mustafa kart <46137737+mustafakart000@users.noreply.github.com>
2025-01-18 01:00:49 +0000

    trigger workflow

--- src/main/java/com/hospital/controller/AdminController.java
+++ src/main/java/com/hospital/controller/AdminController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+
 import com.hospital.dto.AdminByAdminRequest;
 import com.hospital.dto.DoctorRequest;
 import com.hospital.dto.Response.AdminResponseList;
