c777dc33eb8f831d6196b94e115296a52fe694e9
Mustafa kart <mustafakart000@gmail.com>
2025-01-23 20:12:00 +0300

    edit config

--- src/main/java/com/hospital/security/config/SecurityConfig.java
+++ src/main/java/com/hospital/security/config/SecurityConfig.java
@@ -69,10 +69,7 @@ public class SecurityConfig {
                                                 .hasAnyRole(Role.DOCTOR.name(), Role.PATIENT.name(),Role.ADMIN.name(),Role.TECHNICIAN.name())
                                                 .requestMatchers("/api/medicine/get/{id}", "/api/medicine/createAll",
                                                                 "/api/medicine/getAllByDoctorSpeciality/**")
-
-                                                .requestMatchers("/lab-requests/patient/{patientId}/pdfs")
-                                                .hasAnyRole(Role.DOCTOR.name(), Role.PATIENT.name(), Role.ADMIN.name(),
-                                                                Role.TECHNICIAN.name())
+                                                .hasAnyRole(Role.DOCTOR.name(), Role.ADMIN.name())
                                                 .requestMatchers("/api/technician/lab-requests/all")
                                                 .hasAnyRole(Role.DOCTOR.name(), Role.ADMIN.name(),
                                                                 Role.TECHNICIAN.name())
@@ -95,14 +92,8 @@ public class SecurityConfig {
                                                 .requestMatchers("/api/patient/get/{id}", "/api/patient/update/{id}")
                                                 .hasAnyRole(Role.DOCTOR.name(), Role.PATIENT.name(), Role.ADMIN.name(),
                                                                 Role.TECHNICIAN.name())
-<<<<<<< HEAD
-                                                .requestMatchers("/api/medicine/get/{id}", "/api/medicine/createAll",
-                                                                "/api/medicine/getAllByDoctorSpeciality/**")
-=======
                                                 .requestMatchers("/medicine/get/{id}", "/medicine/createAll",
                                                                 "/medicine/getAllByDoctorSpeciality/**")
-
->>>>>>> 13dcae33a7a861807d85f216582ecbaaf430e5ae
                                                 .hasAnyRole(Role.DOCTOR.name(), Role.ADMIN.name())
                                                 .requestMatchers("/api/technician/**")
                                                 .hasAnyRole(Role.DOCTOR.name(), Role.ADMIN.name(),
@@ -183,23 +174,15 @@ public class SecurityConfig {
         @Bean
         public CorsConfigurationSource corsConfigurationSource() {
                 CorsConfiguration configuration = new CorsConfiguration();
-
-                configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://44.204.9.176"));
-                configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-                configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
-                configuration.setAllowCredentials(true);
-
                 configuration.setAllowedOrigins(Arrays.asList(
-                                "http://37.148.209.189:8080", // Backend'in IP adresi ve portu
-                                "http://37.148.209.189", // Backend'in IP adresi
-                                "http://localhost:8080", // Localhost backend
-                                "http://localhost:3000", // Frontend development
-                                "http://healwell.online/"
+                        "http://37.148.209.189:8080",
+                        "http://37.148.209.189",
+                        "http://localhost:8080",
+                        "http://localhost:3000",
+                        "http://healwell.online"
                 ));
                 configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-                configuration.setAllowedHeaders(
-                                Arrays.asList("Authorization", "Content-Type", "Access-Control-Allow-Origin"));
-
+                configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Access-Control-Allow-Origin"));
                 configuration.setExposedHeaders(Arrays.asList("Authorization"));
                 configuration.setAllowCredentials(true);
 
